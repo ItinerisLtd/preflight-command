efd172751446090e2c7a0705296537888e9cb687
Tang Rufus <tangrufus@gmail.com>
2018-08-16 14:01:14 +0800

    Add `HttpsHomeUrl` and `HttpsSiteUrl` checkers

---
+++ src/Checkers/HttpsHomeUrl.php
@@ -0,0 +1,33 @@
+<?php
+declare(strict_types=1);
+
+namespace Itineris\Preflight\Checkers;
+
+use Itineris\Preflight\Config;
+use Itineris\Preflight\ResultInterface;
+use Itineris\Preflight\Validators\HttpsUrls;
+
+class HttpsHomeUrl extends AbstractChecker
+{
+    public const ID = 'https-home-url';
+    public const DESCRIPTION = 'Ensure home URL is secure (HTTPS).';
+    public const FAILURE_MESSAGE = 'Insecure home URL (non-HTTPS)';
+
+    /**
+     * {@inheritdoc}
+     *
+     * @param Config $config The config instance.
+     *
+     * @see https://codex.wordpress.org/Using_Permalinks#Check_for_permalink_structure
+     *
+     * @return ResultInterface
+     */
+    public function run(Config $config): ResultInterface
+    {
+        $validator = HttpsUrls::make($this, static::FAILURE_MESSAGE);
+
+        return $validator->validate(
+            home_url()
+        );
+    }
+}
---
+++ src/Checkers/HttpsSiteUrl.php
@@ -0,0 +1,33 @@
+<?php
+declare(strict_types=1);
+
+namespace Itineris\Preflight\Checkers;
+
+use Itineris\Preflight\Config;
+use Itineris\Preflight\ResultInterface;
+use Itineris\Preflight\Validators\HttpsUrls;
+
+class HttpsSiteUrl extends AbstractChecker
+{
+    public const ID = 'https-site-url';
+    public const DESCRIPTION = 'Ensure site URL is secure (HTTPS).';
+    public const FAILURE_MESSAGE = 'Insecure site URL (non-HTTPS)';
+
+    /**
+     * {@inheritdoc}
+     *
+     * @param Config $config The config instance.
+     *
+     * @see https://codex.wordpress.org/Using_Permalinks#Check_for_permalink_structure
+     *
+     * @return ResultInterface
+     */
+    public function run(Config $config): ResultInterface
+    {
+        $validator = HttpsUrls::make($this, static::FAILURE_MESSAGE);
+
+        return $validator->validate(
+            site_url()
+        );
+    }
+}
