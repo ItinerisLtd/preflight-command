67fc877cc91dfd0de60aff2bb8d07d8964f55c9e
Tang Rufus <tangrufus@gmail.com>
2018-08-18 16:05:32 +0800

    Add `Checkers/InactivePlugins`

---
+++ src/Checkers/InactivePlugins.php
@@ -0,0 +1,54 @@
+<?php
+declare(strict_types=1);
+
+namespace Itineris\Preflight\Checkers;
+
+use Itineris\Preflight\Config;
+use Itineris\Preflight\ResultFactory;
+use Itineris\Preflight\ResultInterface;
+use WP_CLI;
+
+class InactivePlugins extends AbstractChecker
+{
+    public const ID = 'inactive-plugins';
+    public const DESCRIPTION = 'Ensure all plugins are activated.';
+    public const FAILURE_MESSAGE = 'Inactive plugins found:';
+
+    /**
+     * Run the check and return a result.
+     *
+     * Assume the checker is enabled and its config make sense.
+     *
+     * @param Config $config The config instance.
+     *
+     * @return ResultInterface
+     */
+    protected function run(Config $config): ResultInterface
+    {
+        $deactivatedPlugins = WP_CLI::runcommand(
+            'plugin list --status=inactive --fields=name --format=json',
+            [
+                'return' => true,
+                'parse' => 'json',
+                'launch' => true,
+                'exit_error' => false,
+            ]
+        );
+
+        $excludes = $config->compileWhitelist([]);
+        $deactivatedPlugins = array_filter($deactivatedPlugins, function (array $plugin) use ($excludes): bool {
+            return ! in_array($plugin['name'], $excludes, true);
+        });
+
+        $messages = array_map(function (array $plugin): string {
+            return $plugin['name'];
+        }, $deactivatedPlugins);
+
+        return (empty($messages))
+            ? ResultFactory::makeSuccess($this)
+            : ResultFactory::makeFailure(
+                $this,
+                array_merge([self::FAILURE_MESSAGE], $messages)
+            );
+    }
+}
